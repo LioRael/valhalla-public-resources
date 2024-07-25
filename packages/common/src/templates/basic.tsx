46a603e5e611a12fd82c648e19b8a529cf8d3d7a
Leosouthey <621862169@qq.com>
2024-07-25 15:00:11 +0800

    chore: update npm dependencies to latest versions

--- packages/common/src/templates/basic.tsx
+++ packages/common/src/templates/basic.tsx
@@ -1,20 +1,44 @@
+"use client";
+
+import { useEffect } from "react";
+
 import { createTemplate } from "@sacred-craft/valhalla-resource";
-import { ResourceMonacoEditor } from "@sacred-craft/valhalla-resource-components";
+import {
+  ResourceMonacoEditor,
+  ResourceSave,
+  useResourceFileContext,
+} from "@sacred-craft/valhalla-resource-components";
 
 const Editor = () => {
-  return <ResourceMonacoEditor />;
+  const { setRightActions } = useResourceFileContext();
+
+  useEffect(() => {
+    setRightActions(<ResourceSave />);
+  }, [setRightActions]);
+
+  return <ResourceMonacoEditor loading={false} />;
 };
 
 export const basic = createTemplate({
   name: "Basic",
-  matchedPaths: [".*.json$", ".*.ya?ml$", ".*.toml$"],
+  matchedPaths: [
+    ".*.json$",
+    ".*.ya?ml$",
+    ".*.toml$",
+    ".*.java$",
+    ".*.kt$",
+    ".*.properties$",
+    ".*.md$",
+  ],
+  filesOptions: {
+    read: "utf-8",
+  },
   options: {
     render: [
       {
         component: Editor,
         value: "editor",
         label: "Editor",
-        order: 1,
       },
     ],
   },
